cb0e276d4d273919403cef9898d13c36f566a7ba
Cameron Epstein <cameron.a.epstein@gmail.com>
2019-03-06 21:20:47 +0000

    update json set data

--- server/index.js
+++ server/index.js
@@ -6,15 +6,22 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(pino);
 
-const testData = {
+const boilerRoomSetData = {
   "sets":[
     {
       "id": 1,
-      "title": "one",
+      "title": "Anklepants Boiler Room Berlin Live Set",
+      "video_link": "https://www.youtube.com/watch?v=Fb4bCgWkZRc"
     },
     {
       "id": 2,
-      "title": "two",
+      "title": "Moses Boyd's Exodus - Boiler Room In Stereo",
+      "video_link": "https://www.youtube.com/watch?v=6SlTkV4UMPI"
+    },
+    {
+      "id": 3,
+      "title": "Overmono (Tessela & Truss) Boiler Room London Live Set",
+      "video_link": "https://www.youtube.com/watch?v=BpA2n8bvpOk"
     }
   ]
 };
@@ -22,7 +29,7 @@ const testData = {
 
 app.get('/top', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify(testData));
+  res.send(JSON.stringify(boilerRoomSetData));
 })
 
 app.listen(3001, () =>
