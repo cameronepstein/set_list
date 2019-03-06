7a9d82ff4d928d125d7a5135a6b9838cbac4e56f
Cameron Epstein <cameron.a.epstein@gmail.com>
2019-03-06 20:25:33 +0000

    render setList from app.js

--- server/index.js
+++ server/index.js
@@ -26,5 +26,5 @@ app.get('/top', (req, res) => {
 })
 
 app.listen(3001, () =>
-  console.log('Server running on localhost:3001');
+  console.log('Server running on localhost:3001')
 )
---
+++ src/setList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SetList extends Component {  
+  render() {
+    return (
+      <div>
+        <h1>Boiler room stuff</h1>
+      </div>
+    )
+  }
+}
+
+export default SetList;
