d2141830009747b9f3fabffba976966c61a8c75a
Cameron Epstein <cameron.a.epstein@gmail.com>
2019-03-07 18:45:32 +0000

    remove babel and add server tests

--- tests/test.js
+++ tests/test.js
@@ -1,8 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { configure, shallow } from 'enzyme';
-import App from '../src/App.js';
-
 const chai = require('chai');
 const expect = chai.expect;
 const chaihttp = require('chai-http')
@@ -12,12 +7,26 @@ let should = chai.should();
 chai.use(chaihttp);
 
 describe('Server', ()=>{
-  it('should respond to requests with Boiler Room set data', (done) =>{
+  it('should respond to requests', (done) =>{
     chai.request(server)
     .get('/top')
     .end(function(err, res){
       expect(res).to.have.status(200);
+      done();
+    });
+  });
+  it('should respond to requests with Boiler Room set array', (done) =>{
+    chai.request(server)
+    .get('/top')
+    .end(function(err, res){
       expect(res.body.sets).to.be.an('array');
+      done();
+    });
+  });
+  it('should respond to requests with three sets', (done) =>{
+    chai.request(server)
+    .get('/top')
+    .end(function(err, res){
       expect(res.body.sets).to.have.length(3);
       done();
     });
